5696978423fc5fe627d82c31fb76d15ce572feeb
xdVk <xdVkRLD@gmail.com>
2026-03-07 21:11:16 -0600

    backup before i reinstall lol

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -1,1 +1,59 @@
 #pragma once
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <cassert>
+
+enum struct TokenKind : uint8_t {
+    Invalid,
+    EndOfFile,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Semicolon,
+    Number,
+    String,
+    Identifier,
+};
+
+struct Token {
+    Token() : kind(TokenKind::Invalid), line(0), column(0), value("") {}
+    Token(TokenKind kind, size_t line, size_t column, const std::string& value = "")
+        : kind(kind), line(line), column(column), value(value) {}
+    TokenKind kind;
+    size_t line;
+    size_t column;
+    std::string value;
+};
+
+class Lexer {
+public:
+    explicit Lexer(const char* start, const char* end) : start(start), current(start), end(end) {}
+    
+    Lexer(const Lexer&) = delete;
+    Lexer& operator=(const Lexer&) = delete;
+    Lexer(Lexer&&) = delete;
+    Lexer& operator=(Lexer&&) = delete;
+
+    std::vector<Token> lex();
+private:
+    [[nodiscard]] inline char peek() const noexcept {
+        return (current < end ) ? *current : '\0';
+    }
+ 
+    inline void next(size_t n = 1) {
+        // *end represents EOL
+        assert(current + n <= end && "Lexer advanced past end of input"); 
+        current += n;
+    }
+
+    Token lex_number();
+    Token lex_string();
+    Token lex_identifier();
+
+    const char* start;
+    const char* current;
+    const char* end;
+};
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,1 +1,25 @@
 #include "lexer.hpp"
+
+#include <cctype>
+#include <vector>
+
+std::vector<Token> Lexer::lex() {
+    std::vector<Token> tokens;
+    Token token;
+
+    do {
+        while (std::isspace(peek())) {
+            
+        }
+
+
+        if (std::isdigit(peek())) 
+            tokens.emplace_back(lex_number());
+
+    } while (token.kind != TokenKind::EndOfFile);
+    return tokens;
+}
+
+Token Lexer::lex_number() {
+    return Token(TokenKind::Number, 0, 0, "");
+}
