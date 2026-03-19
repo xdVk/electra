b96be286d02375d5c4fe703f6f82193cc00d590d
xdVk <xdvk@gmail.com>
2026-03-18 23:36:27 -0500

    refactor: apply clang-tidy fixes to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "lexer.hpp"
+#include "parser.hpp"
 
 #include <exception>
 #include <format>
@@ -9,30 +10,37 @@
 #include <string>
 
 int main(int argc, const char* argv[]) try {
-    if (argc < 2) throw std::runtime_error("Missing input file.");
+    if (argc < 2) { throw std::runtime_error("Missing input file."); };
 
     const std::string file_name = argv[1];
     std::ifstream source_file(file_name, std::ios::binary | std::ios::ate);
 
-    if (!source_file) throw std::runtime_error(std::format("Failed to open file: {}", file_name));
+    if (!source_file.is_open()) {
+        throw std::runtime_error(std::format("Failed to open file: {}", file_name));
+    }
 
     const std::streamsize source_size = source_file.tellg();
-    if (source_size < 0) throw std::runtime_error("Failed to determine file size.");
+    if (source_size < 0) { throw std::runtime_error("Failed to determine file size."); }
 
     source_file.seekg(0, std::ios::beg);
 
     std::string buffer(static_cast<size_t>(source_size), '\0');
-    if (!source_file.read(buffer.data(), source_size))
+    if (!source_file.read(buffer.data(), source_size)) {
         throw std::runtime_error("Failed to read file.");
+    }
 
     const char* start = buffer.data();
     const char* end = buffer.data() + buffer.size();
 
     electra::Lexer lexer(start, end);
-    auto tokens = lexer.lex();
-    for (const auto& token : tokens) {
-        std::cout << token << '\n';
-    }
+    const auto tokens = lexer.lex();
+    // for (const auto& token : tokens) {
+    //     std::cout << token << '\n';
+    // }
+
+    electra::Parser parser(tokens);
+    const auto ast = parser.parse();
+    ast.print();
 } catch (const std::exception& exception) {
     std::cerr << "Error: " << exception.what() << std::endl;
     return 1;
